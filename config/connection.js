5498bd054591497af7e484ae9ac05fb8c0cf6e37
Moshood Alaran <geekyshood@gmail.com>
2018-11-14 15:41:22 +0100

    changed config to process

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,11 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost', 
-    user : 'root', 
-    password : '', 
+    user : process.env.USERNAME, 
+    password : process.env.PASSWORD, 
     database : 'movies_api'
 });
 
 connection.connect();
-
-// connection.query('SELECT * FROM movies', function(err, results, fields){
-//     if(err) { 
-//         throw new Error("There was an error with the query");
-//     }
-//     console.log("The movies are ", results);
-// });
 
 module.exports = connection;
