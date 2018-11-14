3ab1d842ce6851270245721c67e7f986649c65f5
Moshood Alaran <geekyshood@gmail.com>
2018-11-14 11:16:43 +0100

    added mysql connection

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost', 
+    user : 'root', 
+    password : '', 
+    database : 'movies_api'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM movies', function(err, results, fields){
+    if(err) { 
+        throw new Error("There was an error with the query");
+    }
+    console.log("The movies are ", results);
+});
