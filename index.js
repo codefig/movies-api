1ffce347379ab14758cdaa58ecda7c33ff247d44
Moshood Alaran <geekyshood@gmail.com>
2018-11-22 10:17:29 +0100

    removed spaghetti codes from index.js

--- index.js
+++ index.js
@@ -16,40 +16,6 @@ app.get('/', function(req, res){
     res.status(200).send("Welcome to the Movies API V2.0");
 })
 
-
-// async function createCourse(){
-//     const course = new Course({
-//         name: "Angular Course", 
-//         author : "Mosh Ali", 
-//         tags : ['angular', 'frontend'], 
-//         isPublished : true
-//     }); 
-//     const result = await course.save();
-//     return result;
-// }
-
-
-// app.get('/create', function(req, res){
-//     const response = createCourse();
-//     response.then(result => {
-//         console.log("result : " + result);
-//     }).catch(err => {
-//         console.log(new Error("Something happend"));
-//     })
-//     // console.log("This is the resp: " +response);
-//     // console.log("course createdd");
-//     // return res.send({'response': response});
-// })
-
-// app.get('/', function(req, res, next){
-//     var movies = "";
-//     db.query("SELECT * FROM movies ", function(err, result, fields){
-//         if(err){throw new Error("Error getting all movies" +  err);}
-//         movies = res;
-//         res.send({"error": false, "message" : result});
-//     })
-// })
-
 let port = process.env.PORT || 4000;
 app.listen(port, function(){
     console.log(`Application started on port ${port}`);
