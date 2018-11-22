4214bba63d82d9b00b2116baf2e6abb1bdffeed3
Moshood Alaran <geekyshood@gmail.com>
2018-11-22 10:22:40 +0100

    removed unused modules

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const bodyparser = require('body-parser');
 const app = express()
-// const db = require('./config/connection');
 const moviesRouter = require('./routes/mongoroute');
-
-const Course = require('./config/database');
 
 app.use(express.json());
 app.use(express.urlencoded());
